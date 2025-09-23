1c30283a0853b08fcc2313365b11a714a7484594
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-09-23 01:55:13 +0000

    git commit -m "atualização - 1:30min"

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,18 +1,41 @@
-
-
-class Towel: #Classe
-    def __init__(self, color: str, size: str): #construtor
-        self.color: str = color #atributo
+class Towel:
+    #construtor
+    def __init__(self, color: str, size: str):
+        self.color: str = color
         self.size: str = size
         self.wetness: int = 0
 
-#referencia
-print("Qual a cor da sua toalha e o tamanho?")
-color = input()
-size = input()
-minha: Towel = Towel(color, size) #objeto
-print(f"Sua toalha é {minha.color} e o tamanho é {minha.size}")
+    #aumentar a umidade da toalha.
+    def dry(self, amount: int):
+        max_wetness = self.get_max_wetness()
+        new_wetness = self.wetness + amount
 
+        #valor máximo da umidade
+        if new_wetness > max_wetness:
+            self.wetness = max_wetness
+            print("A toalha está completamente encharcada!")
+        else:
+            self.wetness = new_wetness
+            print(f"A toalha agora tem {self.wetness} de umidade.")
 
+    #torcer a toalha e zerar a umidade
+    def wring_out(self):
+        self.wetness = 0
+        print("Você torceu a toalha. Ela agora está seca.")
 
+    #retorna o valor máximo de umidade
+    def get_max_wetness(self) -> int:
+        if self.size == "P":
+            return 10
+        elif self.size == "M":
+            return 20
+        elif self.size == "G":
+            return 30
+        else:
+            #se o tamanho não for P, M ou G
+            print("Tamanho de toalha inválido, retornando 0.")
+            return 0
 
+    #verifica se a toalha está seca
+    def is_dry(self) -> bool:
+        return self.wetness == 0
