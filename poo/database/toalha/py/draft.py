16766c3ae0a6cef16366d5e882686a01d1765320
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-09-23 14:51:22 +0000

    2 horas

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,41 +1,44 @@
 class Towel:
-    #construtor
-    def __init__(self, color: str, size: str):
-        self.color: str = color
+    def __init__(self, color: str, size: str): # construtor
+        self.color: str = color # atributos
         self.size: str = size
         self.wetness: int = 0
+    
+    def dry(self, amount: int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.isMaxWetness():
+            self.wetness = self.isMaxWetness()
+            print ("tolha não seca")
 
-    #aumentar a umidade da toalha.
-    def dry(self, amount: int):
-        max_wetness = self.get_max_wetness()
-        new_wetness = self.wetness + amount
-
-        #valor máximo da umidade
-        if new_wetness > max_wetness:
-            self.wetness = max_wetness
-            print("A toalha está completamente encharcada!")
-        else:
-            self.wetness = new_wetness
-            print(f"A toalha agora tem {self.wetness} de umidade.")
-
-    #torcer a toalha e zerar a umidade
-    def wring_out(self):
-        self.wetness = 0
-        print("Você torceu a toalha. Ela agora está seca.")
-
-    #retorna o valor máximo de umidade
-    def get_max_wetness(self) -> int:
-        if self.size == "P":
+    def isMaxWetness(self) -> int:
+        if self.size == "P": # early return
             return 10
-        elif self.size == "M":
+        if self.size == "M":
             return 20
-        elif self.size == "G":
+        if self.size == "G":
             return 30
-        else:
-            #se o tamanho não for P, M ou G
-            print("Tamanho de toalha inválido, retornando 0.")
-            return 0
+        return 0 # default return
 
-    #verifica se a toalha está seca
-    def is_dry(self) -> bool:
-        return self.wetness == 0
+    def __str__(self) -> str: # toString
+        return f"Color:{self.color}, Size:{self.size}, Wet:{self.wetness}"
+
+def main ():
+    towel: Towel = Towel() #criar um objeto
+    while True: #loop
+        line: str = input()
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end": #6: ponto de parada
+            break
+        elif args[0] == "new": # color size
+            color: str = args[1]
+            size: str= args[2]
+            towel = Towel(color, size)
+            
+        elif args[0] == "show":
+            print(towel)
+        else: # 7: erro
+            print("fail: comando não encontrado")
+
+main()
+        
