14b48c188811d4ee97a8c249a875bb449d626131
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-09-28 01:22:13 +0000

    1 hora

---
+++ poo/database/carro/py/draft.py
@@ -0,0 +1,7 @@
+class Carro:
+    def __init__(self, passMax: int, gasMax: int): 
+        self.passageiros: int = passageiros
+        self.passMax: int = passMax
+        self.gasMax: int = gasMax
+        self.gas: int = 0
+        self.km: int = 0
