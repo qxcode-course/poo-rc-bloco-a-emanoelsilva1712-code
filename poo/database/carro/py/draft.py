657bb91bb70c261583e9a35f0b5fa4b0801faf8c
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-09-30 14:58:06 +0000

    2 horas

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,16 +1,42 @@
 class Carro:
-    def __init__(self, passMax: int, gasMax: int): 
+    def __init__(self, passageiros: int):
         self.passageiros: int = passageiros
-        self.passMax: int = passMax
-        self.gasMax: int = gasMax
+        self.passMax: int = 2
+        self.gasMax: int = 100
         self.gas: int = 0
         self.km: int = 0 
 
     def __str__(self) -> str:
-        return "pass:{pass}, gas:{gas}, km:{km}"
+        return f"pass: {self.passageiros}, gas: {self.gas}, km: {self.km}"
         
-    def toString (felf) -> str:
+    def enter (self):
+        if self.passageiros >= self.passMax:
+            print("fail: limite de pessoas atingido")
+        else:
+            self.passageiros+=1
+
+    def leave (self):
+        if self.passageiros == 0:
+            print("fail: nao ha ninguem no carro")
+        else:
+            self.passageiros-=1
         
 def main():
-    Carro = Carro("","")
-    while True: 
+    carro = Carro(0)
+    while True:
+        line: str = input()
+        print(f"$" + line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "show":
+            print(carro)
+
+        elif args[0] == "enter":
+            carro.enter()
+
+        elif args[0] == "leave":
+            carro.leave()
+        elif args[0] == "end":
+            break
+
+main()
