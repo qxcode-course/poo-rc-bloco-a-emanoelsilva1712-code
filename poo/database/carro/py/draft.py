72112b5305a46269509b8ed996a2501c54f15e1c
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-09-30 01:46:01 +0000

    40 minutos

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -4,4 +4,13 @@ class Carro:
         self.passMax: int = passMax
         self.gasMax: int = gasMax
         self.gas: int = 0
-        self.km: int = 0
+        self.km: int = 0 
+
+    def __str__(self) -> str:
+        return "pass:{pass}, gas:{gas}, km:{km}"
+        
+    def toString (felf) -> str:
+        
+def main():
+    Carro = Carro("","")
+    while True: 
